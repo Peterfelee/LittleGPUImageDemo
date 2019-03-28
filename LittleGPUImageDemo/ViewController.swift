3374721f20da105b57041f2f98564108aba2a615
peterlee <peterlee@wifi-lee.local>
2019-03-28 15:51:32 +0800

    INIT PRoject

---
+++ LittleGPUImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleGPUImageDemo
+//
+//  Created by peterlee on 2019/3/28.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LittleGPUImageDemoTests/LittleGPUImageDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  LittleGPUImageDemoTests.swift
+//  LittleGPUImageDemoTests
+//
+//  Created by peterlee on 2019/3/28.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import XCTest
+@testable import LittleGPUImageDemo
+
+class LittleGPUImageDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
