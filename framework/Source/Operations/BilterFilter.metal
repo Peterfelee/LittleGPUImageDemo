301af04b473479f409468e0c729a9f5d65cb5951
peterlee <peterlee@wifi-lee.local>
2019-04-08 14:55:46 +0800

    添加简单美颜滤镜

---
+++ framework/Source/Operations/BilterFilter.metal
@@ -0,0 +1,12 @@
+//
+//  BilterFilter.metal
+//  GPUImage_iOS
+//
+//  Created by peterlee on 2019/4/3.
+//  Copyright © 2019 Red Queen Coder, LLC. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
---
+++ framework/Source/Operations/BilterFilter.swift
@@ -0,0 +1,13 @@
+//
+//  BilterFilter.swift
+//  GPUImage_iOS
+//
+//  Created by peterlee on 2019/4/3.
+//  Copyright © 2019 Red Queen Coder, LLC. All rights reserved.
+//
+
+import UIKit
+
+class BilterFilter: BasicOperation {
+
+}
